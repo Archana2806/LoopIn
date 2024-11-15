44f044c01a29415847baaafd1fd08b503f0a7e3e
Archana2806 <archanamohanty2002@gmail.com>
2024-11-16 00:38:11 +0530

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,9 +14,9 @@ export const Footer = () => {
                     </Col>
                     <Col sm={6} className="text-center text-sm-end">
                         <div className="social-icon">
-                            <a href="#"><img src={navIcon1} alt="LinkedIn"/></a>
-                            <a href="#"><img src={navIcon2} alt="Facebook"/></a>
-                            <a href="#"><img src={navIcon3} alt="Instagram"/></a>
+                            <a href="https://www.linkedin.com/"><img src={navIcon1} alt="LinkedIn"/></a>
+                            <a href="https://www.facebook.com/"><img src={navIcon2} alt="Facebook"/></a>
+                            <a href="https://www.instagram.com/"><img src={navIcon3} alt="Instagram"/></a>
                         </div>
                         <p>Copyright 2024. All Right Reserved.</p>
                     </Col>
